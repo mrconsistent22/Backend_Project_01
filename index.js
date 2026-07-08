22228a98de635862b3415c504bde6a3a51234b18
mrconsistent22 <tejass4825@gmail.com>
2026-07-08 22:43:30 +0530

    post request done (patch and delete not done yet)

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const users = require('./MOCK_DATA.json');
 const app = express();
+const fs = require('fs')
 const port = 3000;
+
+//Middleware - Plugin that can be used to modify the request and response objects
+// urlencoded is used to parse the data sent in the request body, it is used when we send data from a form 
+app.use(express.urlencoded({ extended: false }));
 
 //Routes
 
+// this is how  we can render HTML data in the browser
 app.get('/users', (req, res) => {
     const html = `
     <ul>
@@ -16,9 +22,13 @@ app.get('/users', (req, res) => {
 
 
 //REST API
+// '/api' means it will give JSON data instead of HTML data
 app.get('/api/users', (req, res) => {
     return res.json(users);
 })
+
+
+// Dynamic Routing
 
 // app.get('/api/users/:id', (req, res) => {
 //     const id = Number(req.params.id);
@@ -26,10 +36,18 @@ app.get('/api/users', (req, res) => {
 //     return res.json(user);
 // })
 
+
 app.post('/api/users', (req, res) => {
-    // TO DO: Create user
-    return res.json({status: "pending"})
+    // TO DO: Create user - 
+    const body =req.body;
+    console.log('Body: ', body)
+    users.push({...body, id: users.length + 1})
+    fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err, data) => {
+        return res.json({status: "success", id: users.length})
+    })
+    
 })
+
 
 // app.patch('/api/users:id', (req, res) => {
 //     // TO DO: Edit the user with id
@@ -42,18 +60,27 @@ app.post('/api/users', (req, res) => {
 // })
 
 // patch delete and get all are same as they are working with id
-// so we can merge them into one route and use req.method to determine the action
+// so we can merge them into one route and use different methods for each operation
 
 
+
+// Dynamic Routing
+// /api/users/:id - is used for dynamic routing, where :id is a placeholder for the user id
 app.route('/api/users/:id').get((req, res) => {
     const id = Number(req.params.id);
     const user = users.find(user => user.id === id);
     return res.json(user);
 }).patch((req, res) => {
+    // edit user with id
     return res.json({status: "pending"})
 }).delete((req, res) => {
+    // delete user with id
     return res.json({status: "pending"})
 })
+
+
+
+
 
 
 
